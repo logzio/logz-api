13516a39e6610d24c7fb3321b828bdb3c7a1475e
Doron-Bargo <62555360+Doron-Bargo@users.noreply.github.com>
2024-01-01 13:16:22 +0200

    filter only on the relevant headers

--- netlify/functions/proxy.js
+++ netlify/functions/proxy.js
@@ -3,11 +3,17 @@ const fetch = require('node-fetch');
 
 exports.handler = async function (event, context) {
   const { url, method, headers, body } = JSON.parse(event.body);
+  
+  // Extract only the desired headers
+  const filteredHeaders = {
+    'Content-Type': headers['Content-Type'],
+    'X-API-TOKEN': headers['X-API-TOKEN'],
+  };
 
   try {
     const targetResponse = await fetch(url, {
       method,
-      headers,
+      headers: filteredHeaders,
       body,
     });
 
